5e82a5d1e9688077fcde3acaff03032e93942468
Jason Chen <jasonchenhdu@gmail.com>
2016-09-26 22:41:36 +0800

    finish the function for queue

--- HDU/2ProcessState/os2.c
+++ HDU/2ProcessState/os2.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 #include<string.h>
+#include<time.h>
+#include<stdlib.h>
 #define free 0
 #define ready 1
 #define running 2
 #define ts 2 /* time slice */
-
+#define false 0
+#define true 1
 struct PCB {
     int pid;
     int pstate;
@@ -17,26 +20,50 @@ struct Queue{
 	struct PCB* ptail;
 	int num;
 };
+typedef int bool;
 int run(struct PCB*);
 void printPCB(struct PCB*);
 void createPCB(struct Queue*,int);
-
 void append(struct Queue*,struct PCB*);
 void printQueue(struct Queue*);
 void iniQueue(struct Queue*);
+bool isEmpty(struct Queue*);
+bool isSingle(struct Queue*);
+struct PCB* popFirst(struct Queue*);
+struct PCB* popRandom(struct Queue*);
+int getRandomNum();
 int main(){
 	struct Queue freeQueue,readyQueue,runningQueue;
-
+	struct PCB* p;
+	int i;
+	
 	iniQueue(&freeQueue);
 	iniQueue(&readyQueue);
 	iniQueue(&runningQueue);
 	createPCB(&freeQueue,3);
 	createPCB(&freeQueue,2);
-//	printQueue(&freeQueue);
+	createPCB(&freeQueue,5);
+	createPCB(&freeQueue,1);
+	createPCB(&freeQueue,2);
+	createPCB(&freeQueue,3);
+	createPCB(&freeQueue,4);
+	createPCB(&freeQueue,12);
+	createPCB(&freeQueue,11);
+	createPCB(&freeQueue,9);
+	createPCB(&freeQueue,8);
+	for(i=0;i<15;i++){
+		if((p=popRandom(&freeQueue))!=NULL){
+			printf("P%d\n",p->pid);
+		}else{
+			printf("empty!\n");
+		}	
+	}
+	
+	
 }
 
-
 //create PCB with given time,and append to freeQueue
+//后期要修改直接中freeQueue中取 
 void createPCB(struct Queue* queue,int time){
 	static int cnt = 0;
 	struct PCB* newPCB = (struct PCB*)malloc(sizeof(struct PCB));
@@ -47,7 +74,6 @@ void createPCB(struct Queue* queue,int time){
 	newPCB->pnext = NULL;
 	printPCB(newPCB);
 	append(queue,newPCB);
-	printQueue(queue);
 }
 //run pcb and decline time
 int run(struct PCB* pcb){
@@ -74,14 +100,19 @@ void append(struct Queue* queue,struct PCB* pcb){
 	queue->num++;
 }
 //print queue from front to the end
+//输出格式有待优化 
 void printQueue(struct Queue* queue){
 	struct PCB* p = queue->phead;
 
-	while(p != NULL){
-		printf("P%d\t",p->pid);
-		p = p->pnext;
+	if(isEmpty(queue)){
+		printf("It's empty\n");
+	}else{
+		while(p != NULL){
+			printf("P%d\t",p->pid);
+			p = p->pnext;
+		}
+		printf("\n");	
 	}
-	printf("\n");
 }
 //init Queue with some default value
 void iniQueue(struct Queue* queue){
@@ -90,3 +121,66 @@ void iniQueue(struct Queue* queue){
 	queue->num = 0;
 }
 
+//figure out whether the queue is empty
+bool isEmpty(struct Queue* queue){
+	if(queue->num==0)
+		return true;
+	else return false;
+}
+
+//figure out whether the queue only have one element
+bool isSingle(struct Queue* queue){
+	if(queue->num==1)
+		return true;
+	else return false;
+}
+
+//pop the first element of the queue
+struct PCB* popFirst(struct Queue* queue){
+	struct PCB* result = NULL;
+	if(isEmpty(queue)){
+		return result;
+	}else if(isSingle(queue)){
+		result = queue->phead;
+		queue->num--;
+		queue->phead = queue->ptail = NULL;
+		return result;
+	}else{
+		result = queue->phead; 
+		queue->phead = result->pnext;
+		queue->num--;
+		return result;
+	}
+}
+//pop  element of the queue randomly
+struct PCB* popRandom(struct Queue* queue){
+	int n = queue->num;
+	int r = (getRandomNum())%(n>0?n:1);
+	int cnt = 0,i = 0;
+	struct PCB* result = NULL;
+	struct PCB* p = NULL;
+	if(isEmpty(queue)){
+		return result;
+	}else if(r == 0){
+		return popFirst(queue);
+	}else{
+		queue->num--;
+		p = queue->phead;
+		for(i=1;i<cnt;i++){
+			p = p->pnext;
+		}
+		result = p->pnext;
+		p->pnext = result->pnext;
+		return result;
+	}
+}
+//get a integer randomly
+int getRandomNum(){
+	static int pre = 0;
+	
+	if(pre==0)
+		srand(time(NULL));
+	else srand(pre);
+	pre = rand();
+   	return pre;
+}
